0723640ae73a6773c79242005b369316269903da
issackp <appoy007@gmail.com>
2016-09-29 16:10:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var button = document.getElementbyId('counter');
+button.onClick = function(){
+    
+var counter =0;
+    counter+-1;
+    var span= document.getElementbyId('myspan');
+    span.innerHTML=counter.toString();
+};
